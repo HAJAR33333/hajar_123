60d345fb3b9007c139f0e23e575092564e0e61a1
HajarRACHACH <hajar.rachach@adria-bt.com>
2025-11-05 17:34:05 +0100

    enregistre Category et enrichit ProductAdmin (Stock, Category)

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
+
+
+@admin.register(Category)
+class CategorytAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    search_fields = ("name",)
+    prepopulated_fields = {"slugs": ("name",)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "created_at")
+    list_display = ("id", "name", "price", "stock","Category", "created_at")
     search_fields = ("name",)
+    list_filter = ("Category",)
+
